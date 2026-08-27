f0f4b1a67ebab7653847427d3206a35f26ce090e
msrohrbau-lang <msrohrbau@gmail.com>
2026-08-27 16:20:20 +0200

    Use correct Supabase project URL in runtime config

--- api/config.js
+++ api/config.js
@@ -2,7 +2,7 @@ module.exports = function handler(req, res) {
   const clean = value => String(value || '').replace(/\s+/g, '');
   res.setHeader('Cache-Control', 'no-store');
   res.status(200).json({
-    supabaseUrl: clean(process.env.SUPABASE_URL),
+    supabaseUrl: 'https://dbaiwcqoigqgknmtctwl.supabase.co',
     supabaseAnonKey: clean(process.env.SUPABASE_ANON_KEY),
     stripeCheckoutUrl: 'https://buy.stripe.com/3cI6oJcsF7ft7YI4m2cbC00',
     version: '1.3'
