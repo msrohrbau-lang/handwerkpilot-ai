4af9eb08d185446b5b95698b64015ab605744255
msrohrbau-lang <msrohrbau@gmail.com>
2026-08-27 21:04:26 +0200

    Use Stripe sandbox checkout for end-to-end testing

--- api/config.js
+++ api/config.js
@@ -3,7 +3,8 @@ module.exports = function handler(req, res) {
   res.status(200).json({
     supabaseUrl: 'https://dbaiwcqoigqgknmtctwl.supabase.co',
     supabaseAnonKey: 'sb_publishable_8irMEHCYLPzCmMljWAUCaA_L7xJSZlr',
-    stripeCheckoutUrl: 'https://buy.stripe.com/3cI6oJcsF7ft7YI4m2cbC00',
-    version: '1.3'
+    stripeCheckoutUrl: 'https://buy.stripe.com/test_fZu4gBezQ0ffeOp3h55Rm00',
+    stripeMode: 'sandbox',
+    version: '1.3.1'
   });
 };
